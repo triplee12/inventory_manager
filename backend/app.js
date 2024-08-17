72ba02277f53bdf59d11e95bcaa9186d0bd3b817
triplee12 <tripleeoliver@gmail.com>
2024-08-17 10:35:55 +0100

    Refactored code base

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 
@@ -21,3 +25,5 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 app.get("/", (req, res) => {
     res.json({ "message": "Hello, welcome to Inventory." })
 })
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
